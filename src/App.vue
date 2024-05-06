985e3bdb98d139f0751fffb35802a6d0138b394d
GANBAA007 <132818859+GANBAA007@users.noreply.github.com>
2024-05-06 09:31:33 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,206 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="mx-[3.5%]">
+    <div
+      class="max-w-[1440px] place-content-evenly grid grid-cols-3 gap-3 mt-6 mb-32"
+    >
+      <img src="./assets/Landsacpe.png" alt="" />
+      <div>
+        <ul class="grid grid-cols-5 gap-[20%] pt-[18px] pb-6 text-white">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Develop</a></li>
+          <li><a href="#">Community</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="flex justify-center text-canter gap-8 text-white mt-3">
+          <!-- whole box clickable needed -->
+          <li>
+            <div class="py-[4px] px-[10px] rounded-2xl bg-transparent">
+              <a class="leading-4" href="#">LOGIN</a>
+            </div>
+          </li>
+          <li>
+            <div class="py-[4px] px-[20px] rounded-2xl -mr-[20px]">
+              <a class="text-white font-semibold leading-4" href="#">
+                REGISTER
+              </a>
+            </div>
+          </li>
+          <div class="w-[0.5px] max-h-fit bg-[#464646]"></div>
+          <div class="pt-1 -mr-[170px]">
+            <a href="#" class="fa-solid fa-gear"></a>
+          </div>
+        </ul>
+      </div>
+    </div>
+    <div class="flex justify-between bg-[url('./assests/wave1.png')]">
+      <div class="w-[60%] z-0 pb-[160px]">
+        <h1 class="font-extrabold w-fit text-white text-5xl mb-8">
+          Build your portfolio with our AI integrated trading platform
+        </h1>
+        <p class="text-8 leading-8 text-white font-medium mb-11">
+          Revolutionize how you trade on the crypto market
+        </p>
+        <div>
+          <ul class="flex gap-[29px]">
+            <li>
+              <a
+                href="#"
+                class="bg-[#84FFFF] text-black rounded-[32px] block py-[16px] px-14 font-semibold"
+                >Start building</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="bg-transparent border-white border-2 text-white rounded-[32px] block py-[16px] px-14 font-semibold"
+                >Start building</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div class="bg-[url('/assests/wave1.png')]"></div>
+        <img
+          class="fixed blur-lg left-[800px] top-[80px] z-[-1] mr-[3%]"
+          src="./assets/wave1.png"
+          alt=""
+        />
+      </div>
+    </div>
+    <div
+      class="grid grid-cols-3 w-[80%] z-0 mx-auto bg-black/15 pt-5 pb-6 pl-11 pr-5 max-w-fit gap-8 rounded-2xl mb-36"
+    >
+      <div>
+        <h1 class="text-[#006064] font-semibold text-2xl">First slide</h1>
+        <p class="text-white truncate">
+          kasjduhuasbduansdunasudbaiusodnasjndasuiodbausbdiausbdasuobdiyasd
+        </p>
+      </div>
+      <div>
+        <h1 class="text-[#006064] font-semibold text-2xl">Secong slide</h1>
+        <p class="text-white truncate">
+          kasjduhuasbduansdunasudbaiusodnasjndasuiodbausbdiausbdasuobdiyasd
+        </p>
+      </div>
+      <div>
+        <h1 class="text-[#006064] font-semibold text-2xl">Third slide</h1>
+        <p class="text-white truncate">
+          kasjduhuasbduansdunasudbaiusodnasjndasuiodbausbdiausbdasuobdiyasd
+        </p>
+      </div>
+    </div>
+    <div>
+      <img
+        class="w-[100%] justify-center place-content-center mx-auto"
+        src="./assets/Group 10.png"
+        alt=""
+      />
+    </div>
+    <div>
+      <h2
+        class="text-[120px] font-extrabold bg-gradient-to-b from-[#311B92] to-[#006064] via-purple-500 text-transparent bg-clip-text text-center leading-tight"
+      >
+        Features
+      </h2>
+      <p
+        class="text-white font-extrabold text-[64px] text-center justify-center place-content-center bg-transparent mb-[50px] z-[-1]"
+      >
+        Powerful for developes. And fast for everyone
+      </p>
+      <p class="text-white text-[24px] text-center mb-[50px]">
+        Integrate once and never worry about scaling again. Never deal with
+        fragmented Layer 2 systems.
+      </p>
+    </div>
+    <div class="flex align-middle max-w-full mx-auto place-content-center">
+      <div><img src="./assets/Asset 10@2x.png" alt="" /></div>
+      <div class="max-w-[60%] my-auto">
+        <h1 class="text-[#996FF4] font-semibold text-[32px] mb-5">
+          Vivamus sit amet interdum
+        </h1>
+        <p class="text-[24px] text-white">
+          Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate
+          sed. Vestibulum sit amet tortor sit amet libero lobortis.
+        </p>
+      </div>
+    </div>
+    <div class="flex align-middle max-w-full mx-auto place-content-center">
+      <div class="max-w-[60%] my-auto">
+        <h1 class="text-[#996FF4] font-semibold text-[32px] mb-5">
+          Vivamus sit amet interdum
+        </h1>
+        <p class="text-[24px] text-white">
+          Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate
+          sed. Vestibulum sit amet tortor sit amet libero lobortis.
+        </p>
+      </div>
+      <div><img src="./assets/Asset 9@2x.png" alt="" /></div>
+    </div>
+    <div class="flex align-middle max-w-full mx-auto place-content-center">
+      <div><img src="./assets/Asset11.png" alt="" /></div>
+      <div class="max-w-[60%] my-auto">
+        <h1 class="text-[#996FF4] font-semibold text-[32px] mb-5">
+          Vivamus sit amet interdum
+        </h1>
+        <p class="text-[24px] text-white">
+          Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate
+          sed. Vestibulum sit amet tortor sit amet libero lobortis.
+        </p>
+      </div>
+    </div>
+    <div>
+      <h2
+        class="text-[120px] font-extrabold bg-gradient-to-b from-[#311B92] to-[#006064] via-purple-500 text-transparent bg-clip-text text-center leading-tight"
+      >
+        Develops
+      </h2>
+      <p
+        class="text-white font-extrabold text-[64px] text-center justify-center place-content-center bg-transparent mb-[50px] z-[-1]"
+      >
+        3 simple steps Integrate and never worry about scaling again
+      </p>
+      <p class="text-white text-[24px] text-center mb-[80px]">
+        3 simple steps integrate and never worry about scaling again
+      </p>
+    </div>
+    <div class="grid grid-cols-5 justify-center mx-[7%] gap-[80px]">
+      <div class="">
+        <img src="./assets/transaction2 (2).png" alt="" />
+        <div class="">
+          <p class="text-[36px] font-bold text-white text-center">Get coin</p>
+        </div>
+      </div>
+      <div
+        class="flex justify-center items-center bg-[#996FF4] h-[3%] my-auto rounded-lg"
+      ></div>
+      <div class="">
+        <img src="./assets/transaction2.png" alt="" />
+        <div class="">
+          <p class="text-[36px] font-bold text-white text-center">
+            Pick wallet
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex justify-center items-center bg-[#996FF4] h-[3%] my-auto rounded-lg"
+      ></div>
+      <div class="">
+        <img src="./assets/transaction2 (3).png" alt="" />
+        <div class="">
+          <p class="text-[36px] font-bold text-white text-center">
+            Start building
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="table-footer-group"></div> -->
+  </div>
 </template>
 
 <style scoped></style>
